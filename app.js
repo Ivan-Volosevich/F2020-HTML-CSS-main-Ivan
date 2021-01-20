058d05a0a8fae3a717f246a43709701735133ab7
группы стационара <st@itstep.lan>
2021-01-20 21:38:31 +0300

    20.01.2021

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express'); // ЗАПУСК: "node app"
 const app = express();
 const { Client } = require('pg');
+const bodyParser = require('body-parser');
+
 
 const client = new Client({
  connectionString: 'postgres://egsjjxkhdolcat:39a917b7726167cc28cd897518fc10a2f860fe7366a4af7f634bf09f349b4239@ec2-52-208-138-246.eu-west-1.compute.amazonaws.com:5432/deis0sip1foj27',
@@ -28,9 +30,10 @@ client.connect(err => {
 
 app.set('view engine', 'pug');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));         //функции промежуточной обработки | middle where
+app.use(bodyParser.json());                             // { extended: true }
 
-app.get('/', function(req, res) {
+app.get('/', function(req, res) {     // require - запрос; response - ответ
     res.send('Hello');
 });
 
@@ -39,7 +42,6 @@ app.get('/main', function(req, res) {
 });
 
 app.get('/userlist', function (req, res) {
-
     client.query('SELECT * FROM Persons', function(err, response) {
         if (err) {
             res.send('Error!');
@@ -47,12 +49,23 @@ app.get('/userlist', function (req, res) {
             res.render('index', { title: 'WowMan', users: response.rows});
         }
     })
+});
 
-  });
+app.get('/adduser', function(req, res) {
+    res.render('addUser');  //тут addUser = наш шаблон *.pug
+});
 
-  app.get('/adduser', function(req, res) {
-      res.render('addUser');  //тут addUser = наш шаблон *.pug
-  });
+app.post('/createUser', function(req, res) {
+    client.query(`INSERT
+                  INTO Persons (firstname, lastname)
+                  VALUES ('${req.body.firstname}', '${req.body.lastname}')`), (err, result) => {
+                      if (err) {
+                          res.status(500).json({ error: err.stack})
+                      } else {
+                          res.status(200).json({ response: result})
+                      }
+                  }
+});
 
 app.listen(3000, function() {
     console.log('success');
--- public/js/script.js
+++ public/js/script.js
@@ -3,11 +3,36 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('#form'); //поиск с помощью якоря # из *.pug
     const btn = document.querySelector('.btn'); //поиск с помощью класса из *.pug
 
+
     btn.addEventListener('click', function(e) {
         e.preventDefault();
-        console.log(form.elements);
+        let data = {};
+        const elements = [...form.elements];
+        for (let el of elements) {              // for...of
+            if (el.type !== 'submit') {
+                data[el.name] = el.value;
+            }
+        }
+        console.log(data);
+        // console.log(JSON.stringify(data));
+        fetch('/createUser', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            console.log(data);
+            form.insertAdjacentHTML('beforeend', `<p class="alert alert-success">Inserted</p>`);         //!!!ОШИБКА!!!: НЕ ВЫВОДИТ
+        })
     });
 
-    fetch('/ololo') //с помощью get или post app.*** добавить
+
+    
+
+    // fetch('/ololo') //с помощью get или post app.*** добавить
 
 })
