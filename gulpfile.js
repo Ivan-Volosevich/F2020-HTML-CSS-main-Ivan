d696697ad4e3438cce277e122ad569179d1b1bcf
группы стационара <st@itstep.lan>
2021-02-10 21:08:57 +0300

    10.02.2021

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,25 @@ const cssnano = require('gulp-cssnano');
 const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
 
+function fonts() {
+    return gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('./public/fonts/'));
+}
+
 function styles() {
     return gulp.src('src/scss/main.scss')
         .pipe(sass())
         // .pipe(cssnano())
-        .pipe(rename('main.css'))
-        .pipe(gulp.dest('./css/'))
+        .pipe(rename('styles.css'))
+        .pipe(gulp.dest('./public/css'))
         .pipe(browserSync.stream());
 }
 
 function watch() {
     browserSync.init({
         server: {
-           baseDir: "./",
-           index: "./index.html"
+           baseDir: "./public",
+           index: "../index.html"
         }
     });
     gulp.watch('src/scss/**/*.scss', styles)
@@ -26,3 +31,4 @@ function watch() {
 }
 
 exports.watch = watch;
+exports.fonts = fonts;
--- scripts.js
+++ scripts.js
@@ -72,3 +72,84 @@ const weather1 = [
 //     }
 // });
 
+
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const form = document.querySelector('#form'); //поиск с помощью якоря # из *.pug
+    const btn = document.querySelector('.btn'); //поиск с помощью класса из *.pug
+    const statusContainer = document.querySelector('.status'); //либо getElementsByClassName('status'); - но для массива
+
+    const loginForm = document.querySelector('#login-form');
+    const loginButton = document.querySelector('#login-btn');
+
+    if (form) {
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        let data = {};
+        const elements = [...form.elements];
+        for (let el of elements) {              // for...of
+            if (el.type !== 'submit') {
+                data[el.name] = el.value;
+            }
+        }
+        console.log(data);
+        // console.log(JSON.stringify(data));
+        fetch('/createUser', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        }).then((response) => {
+            statusContainer.innerHTML = '';
+            return Promise.all([response.ok, response.json()]);
+        })
+        .then((data) => {
+            console.log(data);
+            if (!data[0]) {
+                throw Error(data[1].error);
+            }
+            statusContainer.insertAdjacentHTML('beforeend', `<p class="alert alert-success">Inserted</p>`);
+            setTimeout(() => {
+                statusContainer.innerHTML = '';
+            }, 3000);
+        }).catch((error) => {
+            console.log(error);
+            statusContainer.insertAdjacentHTML('beforeend', `<p class="alert alert-danger">${error}</p>`);
+            setTimeout(() => {
+                statusContainer.innerHTML = '';
+            }, 3000);
+        })
+    });
+    }
+
+    loginButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        let data = {};
+        const elements = [...loginForm.elements];
+        for (let el of elements) {              // библиотека axios
+            if (el.type !== 'submit') {
+                data[el.name] = el.value;
+            }
+        }
+        fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(data)
+        })
+        .then((res) => res.json())
+        .then((res) => console.log(res));
+
+    });
+
+    
+
+});
